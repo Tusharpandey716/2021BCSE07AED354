94e5079f169f2731373d0e339333f35c1202c0fc
Tushar pandey <139625489+Tusharpandey716@users.noreply.github.com>
2024-09-08 15:51:35 +0530

    Add files via upload

---
+++ 2021BCSE07AED354/script.js
@@ -0,0 +1,14 @@
+// Smooth Scrolling to Sections
+$(document).ready(function(){
+    $('a').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
